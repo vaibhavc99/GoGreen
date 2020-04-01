02b29e04f62aead89bc2502fb3d24990d10c2abf
Vaibhav Chaudhary <49322269+vaibhavc99@users.noreply.github.com>
2020-04-01 16:56:38 +0530

    Update process.py

--- process.py
+++ process.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageFilter, ImageEnhance
 import cv2
 import numpy as np
 
-image = Image.open('jalgaoncenter.jpg')
+image = Image.open('london_ground.jpg')
 width, height = image.size
 if (width == 1280 and height == 720):
     # Mask Image
